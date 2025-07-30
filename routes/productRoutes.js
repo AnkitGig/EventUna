c36035e6b8c4efc65a122fad0ada13f98a358a58
palash2397 <palashpatil55@gmail.com>
2025-07-30 12:48:41 +0530

    crud-product

--- routes/productRoutes.js
+++ routes/productRoutes.js
@@ -7,13 +7,16 @@ const { uploadProductPhoto } = require('../middleware/merchantMulter');
 // Category routes
 router.post('/category', protect, productController.addCategory);
 router.get('/categories', protect, productController.getCategories);
+router.delete('/category', protect, productController.deleteCategory)
 
 // Subcategory routes
 router.post('/subcategory', protect, productController.addSubcategory);
 router.get('/subcategories',protect, productController.getSubcategories);
+router.delete('/subcategory', protect, productController.deleteSubcategory)
 
 // Product routes
 router.post('/product', protect, uploadProductPhoto.array('photo'), productController.addProduct);
 router.get('/products', protect, productController.getProducts);
+router.delete('/product', protect, productController.deleteProduct)
 
 module.exports = router;
